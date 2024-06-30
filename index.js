72538ca24e299b8b8673ee149ecb3d4b4a345d10
Jared Glenn <jared@Rebekahs-MacBook-Pro.local>
2024-06-30 12:00:00 -0600

    add ball movement

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 class Game {
     constructor() {
         this.isRunning = true;
-        this.ballX = 50;
-        this.ballY = 50;
+        this.ballX = 300;
+        this.ballY = 300;
         this.ballXDir = Math.random() < 0.5 ? 1 : -1;
         this.ballYDir = Math.random() < 0.5 ? 1 : -1;;
+        this.ballSpeed = 2;
 
-        this.ballElement = document.getElementsById("ball");
+        this.ballElement = document.getElementById('ball');
     }
 
     run() {
@@ -24,16 +25,18 @@ class Game {
     }
 
     moveBall() {
-        if (this.ballX <= 0 || this.ballX >= 100) {
+        if (this.ballX <= 0 || this.ballX >= 500) {
             this.ballXDir *= -1;
         }
-        if (this.ballY <= 0 || this.ballY >= 100) {
+        if (this.ballY <= 0 || this.ballY >= 400) {
             this.ballYDir *= -1;
         }
-        this.ballX = this.ballX + this.ballXDir;
-        this.ballY = this.ballY + this.ballYDir;
+        this.ballX = this.ballX + this.ballXDir * this.ballSpeed;
+        this.ballY = this.ballY + this.ballYDir * this.ballSpeed;
 
         console.log(`Ball position: ${this.ballX}, ${this.ballY}`);
+        this.ballElement.style.left = `${this.ballX}px`;
+        this.ballElement.style.top = `${this.ballY}px`;
     };
 }
 
