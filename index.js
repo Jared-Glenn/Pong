37b45a129592330406c61b2e4be32bd2862965ad
Jared-Glenn <jared_glenn@yahoo.com>
2024-06-30 12:00:00 -0600

    add css and game logic

--- index.js
+++ index.js
@@ -2,25 +2,42 @@
 
 class Game {
     constructor() {
-        isRunning: true;
-        ballX: 50;
-        ballY: 50;
-        ballXDir: Math.random;
-        ballYDir: 1;
+        this.isRunning = true;
+        this.ballX = 50;
+        this.ballY = 50;
+        this.ballXDir = Math.random() < 0.5 ? 1 : -1;
+        this.ballYDir = Math.random() < 0.5 ? 1 : -1;;
+
+        this.ballElement = document.getElementsById("ball");
     }
 
     run() {
-        while(this.isRunning) {
+        const animate = () => {
+            if (!this.isRunning) return;
+
             this.moveBall();
+
+            requestAnimationFrame(animate);
         }
+
+        animate();
     }
 
     moveBall() {
-        
+        if (this.ballX <= 0 || this.ballX >= 100) {
+            this.ballXDir *= -1;
+        }
+        if (this.ballY <= 0 || this.ballY >= 100) {
+            this.ballYDir *= -1;
+        }
+        this.ballX = this.ballX + this.ballXDir;
+        this.ballY = this.ballY + this.ballYDir;
+
+        console.log(`Ball position: ${this.ballX}, ${this.ballY}`);
     };
 }
 
 
-const newGame = Game();
+const newGame = new Game();
 
 newGame.run();
